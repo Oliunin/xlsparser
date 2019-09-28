ac5f6719eacb6715acbcc1a3d235cc9ea111f9d4
Oliunin <55708721+Oliunin@users.noreply.github.com>
2019-09-28 19:38:00 +0500

    some changes

--- xlsHouseCounter.py
+++ xlsHouseCounter.py
@@ -4,14 +4,12 @@ import sqlite3
 conn = sqlite3.connect('xlsparser.db')
 cur = conn.cursor()
 
-def sql_count_query()
-    cur.execute("SELECT COUNT(Comp_id) from House where Comp_id=%s")
-    Icount=cur.fetchone()[0]
 cur.execute("SELECT PK from Company")
 lPK=cur.fetchall()
 print(lPK)
 for pk in lPK:
     ipk=pk[0]
     print(ipk)
-    sql_count_query(ipk)
+    cur.execute("SELECT COUNT(Comp_id) from House where Comp_id=%s"),(ipk)
+    Icount=cur.fetchone()[0]
     print(Icount)
