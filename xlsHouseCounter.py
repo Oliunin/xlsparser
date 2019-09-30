bddf7b2e438f113f2b67d2715b88eeabb76f81f2
Oliunin <ivan220886@gmail.com>
2019-09-30 12:28:54 +0500

    Counter update

--- xlsHouseCounter.py
+++ xlsHouseCounter.py
@@ -3,13 +3,16 @@ import sqlite3
 
 conn = sqlite3.connect('xlsparser.db')
 cur = conn.cursor()
-
+Housecount = "SELECT COUNT(Comp_id) from House where Comp_id=?"
 cur.execute("SELECT PK from Company")
 lPK=cur.fetchall()
-print(lPK)
+#print(lPK)
 for pk in lPK:
     ipk=pk[0]
-    print(ipk)
-    cur.execute("SELECT COUNT(Comp_id) from House where Comp_id=%s"),(ipk)
+    print(type(ipk),ipk)
+    cur.execute(Housecount,(ipk,))
     Icount=cur.fetchone()[0]
     print(Icount)
+    print(type(Icount),Icount)
+    cur.execute("INSERT OR REPLACE INTO Company(comp_house_count) VALUES (?)",( Icount,))
+conn.commit()
